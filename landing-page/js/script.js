c2569daba0c6a1d78cdf80f44d4731163d11cc6d
Luanna88 <luanna.souza@maisunifacisa.com.br>
2025-08-25 16:05:15 -0300

    Corrijindo css e adicionado responsividade
    Signed-off-by: Luanna88 <luanna.souza@maisunifacisa.com.br>

--- landing-page/js/script.js
+++ landing-page/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
+
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   });
